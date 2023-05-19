0ff31706452cf20bfec12b64f91c3c9ce242addf
mtiongco <49075968+mtiongco@users.noreply.github.com>
2023-05-19 15:43:51 -0500

    1st update to apiout_topandas.py

--- apioutput_topandas.py
+++ apioutput_topandas.py
@@ -8,6 +8,7 @@ Created on Fri May 19 13:25:23 2023
 from steam import Steam
 from decouple import config
 import pandas as pd
+import numpy as np
 
 with open("myapikey.txt", "r") as file:
     myapikey=file.read()
@@ -21,6 +22,11 @@ testid='76561198039393086'
 
 testfl=steam.users.get_user_friends_list(testid)
 
+testflids=[]
+
+for i in range(len(testfl['friends'])):
+    testflids.append(testfl['friends'][i]['steamid'])
+
 testgamelist=steam.users.get_owned_games(testid)
 
 appids=[]
@@ -31,7 +37,27 @@ for i in range(testgamelist['game_count']):
     appids.append(testgamelist['games'][i]['appid'])
     playtimes.append(testgamelist['games'][i]['playtime_forever'])
     
-#make player ids be the index
-#game ids be the columns
-#entries will be the playtime
-#oh god do we have to do a loop
+#make game ids the rows
+#make player ids the columns
+#entries will be the total playtime in that game
+#This is for easier entry into the dataframe, then we can transpose (hopefully)
+
+testdf=pd.DataFrame({int(testid):playtimes},index=appids)
+
+appidstemp=[]
+playtimestemp=[]
+gamelisttemp=steam.users.get_owned_games(testflids[13])
+
+for i in range(gamelisttemp['game_count']):
+    appidstemp.append(gamelisttemp['games'][i]['appid'])
+    playtimestemp.append(gamelisttemp['games'][i]['playtime_forever'])
+   
+testdf[int(testflids[13])]='NO' #preemptively sets all games already in the df to 'NO - Not Owned'
+    
+#Now loop to figure out which games have already been added, if not, add a new
+# entry.  Should also go back and add 'Not Owned' or something to games 
+# not owned.
+
+for appid in appidstemp:
+    if appid in testdf.index:
+        testdf[int(testflids[13])][appid]='YES' #later will figure out how to put playtimes in
