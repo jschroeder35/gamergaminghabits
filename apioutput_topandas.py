278b765fe4e4c9b32acaa0d609ce2e466dbd25bd
mtiongco <49075968+mtiongco@users.noreply.github.com>
2023-05-19 17:59:18 -0500

    apiout_topandas.py working with list of my friends

--- apioutput_topandas.py
+++ apioutput_topandas.py
@@ -22,6 +22,10 @@ testid='76561198039393086'
 
 testfl=steam.users.get_user_friends_list(testid)
 
+#initial test of 3 people
+#testflids=['76561198009190429','76561198048895718','76561198014795926']
+
+#whole friend list 
 testflids=[]
 
 for i in range(len(testfl['friends'])):
@@ -42,22 +46,36 @@ for i in range(testgamelist['game_count']):
 #entries will be the total playtime in that game
 #This is for easier entry into the dataframe, then we can transpose (hopefully)
 
-testdf=pd.DataFrame({int(testid):playtimes},index=appids)
+testdf=pd.DataFrame({int(testid):playtimes},index=appids) #initialize the df
 
-appidstemp=[]
-playtimestemp=[]
-gamelisttemp=steam.users.get_owned_games(testflids[13])
-
-for i in range(gamelisttemp['game_count']):
-    appidstemp.append(gamelisttemp['games'][i]['appid'])
-    playtimestemp.append(gamelisttemp['games'][i]['playtime_forever'])
-   
-testdf[int(testflids[13])]='NO' #preemptively sets all games already in the df to 'NO - Not Owned'
+for j in range(len(testflids)):
+    appidstemp=[]
+    playtimestemp=[]
+    gamelisttemp=steam.users.get_owned_games(testflids[j])
     
-#Now loop to figure out which games have already been added, if not, add a new
-# entry.  Should also go back and add 'Not Owned' or something to games 
-# not owned.
-
-for appid in appidstemp:
-    if appid in testdf.index:
-        testdf[int(testflids[13])][appid]='YES' #later will figure out how to put playtimes in
+    #in case of empty game list
+    if gamelisttemp=={}:
+        continue
+    
+    for i in range(gamelisttemp['game_count']):
+        appidstemp.append(gamelisttemp['games'][i]['appid'])
+        playtimestemp.append(gamelisttemp['games'][i]['playtime_forever'])
+       
+    testdf[int(testflids[j])]='NO' #preemptively sets all games already in the df to 'NO - Not Owned'
+        
+    playtimestemp=pd.Series(playtimestemp,index=appidstemp)
+    
+    #Now loop to figure out which games have already been added, if not, add a new
+    # entry.  Should also go back and add 'Not Owned' or something to games 
+    # not owned.
+        
+    for appid in appidstemp:
+        if appid in testdf.index:
+            #enter the playtime if game alreaady exists in df
+            testdf[int(testflids[j])][appid]=playtimestemp[appid]
+        else: #create the entry in the df
+            newrow=['NO']*(len(testdf.columns)-1) #assigns No('not owned' to previous ids)
+            newrow.append(playtimestemp[appid])
+            testdf.loc[appid]=newrow
+    
+    
